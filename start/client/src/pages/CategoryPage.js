f5c89c14680bc1fa8f239e628b7dde21b3f75bbd
JBR90 <blakeage77@googlmail.com>
2021-11-25 10:39:12 +0000

    react mutation

--- start/client/src/pages/CategoryPage.js
+++ start/client/src/pages/CategoryPage.js
@@ -1,24 +1,47 @@
-import React from 'react'
-import { useParams } from "react-router-dom"
-import { Container } from 'react-bootstrap'
-import CardDisplay from '../components/CardDisplay/CardDisplay'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import CardDisplay from "../components/CardDisplay/CardDisplay";
+import { useQuery, gql } from "@apollo/client";
+
+const CATEGORY_QUERY = gql`
+  query ($slug: String!) {
+    category(slug: $slug) {
+      animals {
+        title
+        slug
+        id
+        price
+        image
+      }
+    }
+  }
+`;
 
 function CategoryPage() {
-    
-    const { slug } = useParams()
+  const { slug } = useParams();
 
-    return (
-        <div className="py-5">
-            <Container>
-                <h1 className="text-capitalize">
-                    {}
-                    <CardDisplay 
-                        animals={[]}
-                    />
-                </h1>
-            </Container>
-        </div>
-    )
+  const { loading, data, error } = useQuery(CATEGORY_QUERY, {
+    variables: {
+      slug,
+    },
+  });
+  console.log(data);
+
+  if (loading) {
+    return <div>Laodin....</div>;
+  }
+
+  return (
+    <div className="py-5">
+      <Container>
+        <h1 className="text-capitalize">
+          {slug}
+          <CardDisplay animals={data.category.animals} />
+        </h1>
+      </Container>
+    </div>
+  );
 }
 
-export default CategoryPage
+export default CategoryPage;
--- start/client/src/pages/LandingPage.js
+++ start/client/src/pages/LandingPage.js
@@ -2,38 +2,79 @@ import React from "react";
 import MainHero from "../components/MainHero/MainHero";
 import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay";
 import CardDisplay from "../components/CardDisplay/CardDisplay";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, useMutation, gql } from "@apollo/client";
 
 const ANIMALS_QUERY = gql`
   {
     animals {
+      image
       id
+      title
+      price
       slug
       image
-      title
-      rating
-      price
-      description
-      stock
-      onSale
     }
   }
 `;
+
+const ADD_ANIMAL_MUTATION = gql`
+  mutation (
+    $image: String!
+    $category: String!
+    $title: String!
+    $stock: Int!
+    $price: String!
+    $description: [String!]!
+    $rating: Float
+    $slug: String!
+  ) {
+    addAnimal(
+      image: $image
+      category: $category
+      title: $title
+      stock: $stock
+      price: $price
+      description: $description
+      rating: $rating
+      slug: $slug
+    ) {
+      id
+    }
+  }
+`;
+
 function LandingPage() {
   const { loading, error, data } = useQuery(ANIMALS_QUERY);
-  console.log(ANIMALS_QUERY);
-  if (loading) {
-    return <div>Loading....</div>;
-  }
+  const [addAnimal] = useMutation(ADD_ANIMAL_MUTATION);
 
-  if (error) {
-    return <div>Error...{data.error}</div>;
-  }
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>Error...{error.message}.</div>;
+
   return (
     <div>
       <MainHero />
       <CategoryDisplay />
-      {!loading && <CardDisplay animals={data.animals} />}
+      <CardDisplay animals={data.animals} />
+      <button
+        onClick={() => {
+          addAnimal({
+            variables: {
+              image: "ostrich",
+              category: "1",
+              title: "This is a really cool ostrich",
+              stock: 13,
+              price: "32,333",
+              description: ["das"],
+              rating: 3.5,
+              slug: "ostrich",
+            },
+          });
+        }}
+      >
+        {" "}
+        Add an Ostrich{" "}
+      </button>
     </div>
   );
 }
